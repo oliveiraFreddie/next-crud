da918009324275fbfeba0099076852649036675c
Frederico Oliveira <135251274+oliveiraFreddie@users.noreply.github.com>
2023-07-07 11:34:32 +0100

    next-crud commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulário";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+
+import useClientes from "../hooks/useClientes";
+ 
+export default function Home() {
+
+  const { 
+    cliente, 
+    clientes, 
+    novoCliente,
+    salvarCliente, 
+    selecionarCliente, 
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela 
+  } = useClientes()
+
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 to-blue-600
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        {tabelaVisivel  ? (
+          <>
+            <div className="flex justify-end">
+              <Botao className="mb-4" cor="green" 
+                onClick={novoCliente}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
+          />
+        )}
+      </Layout>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  safelist: [
+    'from-green-400',
+    'from-blue-400',
+    'from-gray-400',
+    'to-gray-700',
+    'to-blue-700',
+    'to-green-700',
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
