cb5a048f415b52c12b89208a2cf57fb0dae3a424
Frederico Oliveira <135251274+oliveiraFreddie@users.noreply.github.com>
2023-07-08 07:59:11 +0100

    config.ts

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -5,7 +5,7 @@ if(!firebase.apps.length) {
     firebase.initializeApp({
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectid: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     })
 }
 
